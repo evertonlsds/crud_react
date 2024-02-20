d6ede24e251d77ee7c0ad0b448e554bbc75a41f0
evertonlsds <everton0102@gmail.com>
2024-02-19 23:32:51 -0300

    login page layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,20 @@
+import { Hammer } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>autenticação</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Hammer className="h-8 w-8" />
+          <span className="font-semibold">Construction Software</span>
+        </div>
+        <footer className="text-sm">
+          Painel &copy; All rights reserved by Construction Software -{' '}
+          {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,36 @@
+import { Label } from '@radix-ui/react-label'
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export function SingIn() {
-  return <h1>SingIn</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className=" flex w-[350px] flex-col  justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-3xl font-bold tracking-tight">Login</h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas Obras
+            </p>
+          </div>
+          <form className="space-y-4">
+            <div className="space-y-2 font-semibold">
+              <Label>Usuário</Label>
+              <Input id="email" type="email" />
+            </div>
+            <div className="space-y-2 font-semibold">
+              <Label>Senha</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
