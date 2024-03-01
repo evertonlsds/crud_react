9ee8ddf36831d62967db5cb11e09253a6df54e85
evertonlsds <everton0102@gmail.com>
2024-03-01 11:23:20 -0300

    added sign-up to register user

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,4 +1,3 @@
-import { Ghost } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
@@ -9,28 +8,28 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z.string().email(),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignUpForm = z.infer<typeof signUpForm>
 
-export function SignIn() {
+export function SignUp() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignUpForm>()
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
-      toast.success('Enviamos um link de autenticação para seu email', {
-        action: {
-          label: 'Reenviar',
-          onClick: () => handleSignIn(data),
-        },
-      })
+      //   toast.success('Enviamos um link de autenticação para seu email', {
+      //     action: {
+      //       label: 'Reenviar',
+      //       onClick: () => handleSignUp(data),
+      //     },
+      //   })
     } catch {
       toast.error('Credenciais inválidas')
     }
@@ -38,26 +37,26 @@ export function SignIn() {
 
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title="Cadastro" />
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-up">Novo Usuário</Link>
+          <Link to="/sign-in">Fazer Login</Link>
         </Button>
         <div className=" flex w-[350px] flex-col  justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-3xl font-bold tracking-tight">Login</h1>
-            <p className="text-sm text-muted-foreground">
-              Acompanhe suas Obras
-            </p>
+            <h1 className="text-3xl font-bold tracking-tight">
+              Cadastre uma conta
+            </h1>
+            {/* <p className="text-sm text-muted-foreground">Cadastre um email</p> */}
           </div>
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="space-y-2 font-semibold">
-              <Label htmlFor="email">Usuário</Label>
+              <Label htmlFor="email">Novo Usuário</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
-              Acessar
+              Finalizar Cadastro
             </Button>
           </form>
         </div>
