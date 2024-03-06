9541966328a8700523a09dbc23e68f8c862f8074
evertonlsds <everton0102@gmail.com>
2024-03-06 16:38:45 -0300

    creating header component and dark theme

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import { Hammer, HardHat, Home } from 'lucide-react'
+
+import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
+import { Separator } from './ui/separator'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Hammer className="h-7 w-7" />
+
+        <Separator orientation="vertical" className="h-6" />
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="w-4/ h-4" />
+            Inicio
+          </NavLink>
+          <NavLink to="/construction">
+            <HardHat className="w-4/ h-4" />
+            Obras
+          </NavLink>
+        </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  )
+}
